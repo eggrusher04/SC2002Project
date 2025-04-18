6ab8f6ecd9b6cdf03542bad8a95a75098b8eaa06
jrnai <naijunnrong@gmail.com>
2025-04-18 11:08:39 +0800

    done the hdbmanager part

--- V2/Employees.java
+++ V2/Employees.java
@@ -1,7 +1,39 @@
 public abstract class Employees implements Users {
 
-	private int staffID;
-	private String role;
+	
+
+    private String nric;
+    private String password;
+    private int age;
+    private boolean maritalStatus;
+
+    private int staffID;
+    private String role;
+
+	public Employees( String nric, String password, int age, boolean maritalStatus, int staffID, String role) {
+        this.nric = nric;
+        this.password = password;
+        this.age = age;
+        this.maritalStatus = maritalStatus;
+        this.staffID = staffID;
+        this.role = role;
+    }
+
+	@Override public String getName()            { return name; }
+    @Override public String getNRIC()            { return nric; }
+    @Override public String getPassword()        { return password; }
+    @Override public int getAge()                { return age; }
+    @Override public boolean getMaritalStatus()  { return maritalStatus; }
+
+    @Override
+    public boolean login(String nric, String pw) {
+        return this.nric.equals(nric) && this.password.equals(pw);
+    }
+
+    @Override
+    public void changePassword(String newPw) {
+        this.password = newPw;
+    }
 
 	/**
 	 * 
